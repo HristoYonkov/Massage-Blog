b7aadff49d78e14cfd6b4ad7c2ff728c4d763ac7
Hristo Yonkov <hristopturs@gmail.com>
2024-03-09 14:29:15 +0200

    Create About component

---
+++ app/src/components/About/About.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import  "./About.scss";
+
+const About: React.FC = () => {
+    return (
+        <section id="about">
+            <div className="container">
+                <h2>About Me</h2>
+                <p>Welcome to Relax & Rejuvenate Massage Therapy! We specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
