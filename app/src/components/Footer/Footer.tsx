cf5aefef3a5e843775f2ece6f54fa81b3d3d63a5
Hristo Yonkov <hristopturs@gmail.com>
2024-03-09 14:29:25 +0200

    Create Footer.tsx

---
+++ app/src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import  "./Footer.scss";
+
+const Footer: React.FC = () => {
+
+    return (
+        <footer>
+            <div className="container">
+                <p>&copy; 2024 Relax & Rejuvenate Massage Therapy</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
