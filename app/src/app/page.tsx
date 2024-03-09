47ee9e658ead4d68833286b51ae638de87b29f3e
Hristo Yonkov <hristopturs@gmail.com>
2024-03-09 15:14:01 +0200

    add scroll to top button

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,9 +2,12 @@ import About from "@/components/About/About";
 import  "./page.scss";
 
 export default function Home() {
+  
+  
   return (
     <main>
       <About />
+      <a href="#" className="scroll-top-button">&#8648;</a>
     </main>
   );
 }
