945bf1ea1b1499b8d49e26e5a9e64f517b0a0943
Hristo Yonkov <hristopturs@gmail.com>
2024-03-09 16:28:48 +0200

    add button for scrolling to top and function for smooth behavior

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,13 +1,44 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
 import About from "@/components/About/About";
-import  "./page.scss";
+import "./page.scss";
 
 export default function Home() {
-  
-  
-  return (
-    <main>
-      <About />
-      <a href="#" className="scroll-top-button">&#8648;</a>
-    </main>
-  );
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        if (scrollY > 100) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
+    return (
+        <main>
+            <About />
+            <button
+                className={`scroll-top-button ${isVisible ? 'show' : ''}`}
+                onClick={scrollToTop}>
+                &#8648;
+            </button>
+        </main>
+    );
 }
