89d5ff6c5cbc819df36841d66925c243fa7adef0
Hristo Yonkov <hristopturs@gmail.com>
2024-03-10 17:14:06 +0200

    add mobile nav toaster

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 
-import  "./Header.scss";
+import "./Header.scss";
 import logo from '../../images/milen-logo.png';
 
 interface HeaderProps {
@@ -18,13 +18,25 @@ const Header: React.FC = () => {
                 <div className="logo">
                     <Image src={logo} alt="Relax & Rejuvenate Logo" />
                 </div>
-                <nav>
+                <nav className="nav">
                     <ul>
                         <li><a href="#about">About</a></li>
                         <li><a href="#services">Services</a></li>
                         <li><a href="#contact">Contact</a></li>
                         <li><a href="#book">Book Massage</a></li>
                     </ul>
+                </nav>
+                <nav className="toaster-nav">
+                    <input type="checkbox" id="toaster-toggle" />
+                    <label htmlFor="toaster-toggle" className="toaster-toggle-label"><span></span><span></span></label>
+                    <div className="toaster-nav-links">
+                        <ul>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#services">Services</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                            <li><a href="#book">Book Massage</a></li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
         </header>
