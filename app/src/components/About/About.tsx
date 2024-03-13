8319e37942722311a5c5d0f10ab5a92f76f947d3
Hristo Yonkov <hristopturs@gmail.com>
2024-03-13 17:23:56 +0200

    update description

--- app/src/components/About/About.tsx
+++ app/src/components/About/About.tsx
@@ -8,7 +8,7 @@ const About: React.FC = () => {
         <section id="about">
             <div className="container">
                 <h2>About Me</h2>
-                <p>Welcome to Relax & Rejuvenate Massage Therapy! We specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
+                <p>Welcome to Relax & Rejuvenate Massage Therapy! I'm specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
             </div>
         </section>
     )
