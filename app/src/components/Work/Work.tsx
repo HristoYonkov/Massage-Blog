3523599d9a1ccc28cdff1756ee13aa543a932748
Hristo Yonkov <hristopturs@gmail.com>
2024-03-16 15:46:50 +0200

    add a new work component section

---
+++ app/src/components/Work/Work.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import "./Work.scss";
+
+const Work: React.FC = () => {
+
+    return (
+        <section id="services">
+            <h2>Services</h2>
+            <div className="services-container">
+                <div className="img-container-services">
+                    <img src="https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cPl9Faevjc4AX-WZeO-&_nc_ht=scontent.fsof8-1.fna&oh=00_AfBRFVjVmSBHjWK5pw8Cj8Pn1Lt_qjuEhyJYwaHX4Cejgg&oe=65FA62F6" alt="about-image" />
+                </div>
+                <article className="article-container-services">
+                    <h2>Swedish Massage</h2>
+                    <p>Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
+                </article>
+            </div>
+            <div className="services-container">
+                <div className="img-container-services">
+                    <img src="https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cPl9Faevjc4AX-WZeO-&_nc_ht=scontent.fsof8-1.fna&oh=00_AfBRFVjVmSBHjWK5pw8Cj8Pn1Lt_qjuEhyJYwaHX4Cejgg&oe=65FA62F6" alt="about-image" />
+                </div>
+                <article className="article-container-services">
+                    <h2>Deep Tissue Massage</h2>
+                    <p>Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
+                </article>
+            </div>
+            <div className="services-container">
+                <div className="img-container-services">
+                    <img src="https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cPl9Faevjc4AX-WZeO-&_nc_ht=scontent.fsof8-1.fna&oh=00_AfBRFVjVmSBHjWK5pw8Cj8Pn1Lt_qjuEhyJYwaHX4Cejgg&oe=65FA62F6" alt="about-image" />
+                </div>
+                <article className="article-container-services">
+                    <h2>Hot Stone Massage</h2>
+                    <p>Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
+                </article>
+            </div>
+            <div className="services-container">
+                <div className="img-container-services">
+                    <img src="https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cPl9Faevjc4AX-WZeO-&_nc_ht=scontent.fsof8-1.fna&oh=00_AfBRFVjVmSBHjWK5pw8Cj8Pn1Lt_qjuEhyJYwaHX4Cejgg&oe=65FA62F6" alt="about-image" />
+                </div>
+                <article className="article-container-services">
+                    <h2>Suction Cups</h2>
+                    <p>Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
+                </article>
+            </div>
+        </section>
+
+    )
+}
+
+export default Work;
