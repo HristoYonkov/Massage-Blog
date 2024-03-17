a739c0107cdb917ca09c5557826d44310613885b
Hristo Yonkov <hristopturs@gmail.com>
2024-03-17 16:00:51 +0200

    Create aboutSliderData.ts

---
+++ app/src/data/aboutSliderData.ts
@@ -0,0 +1,23 @@
+interface aboutSectionData {
+    imgSrc: string,
+    title: string,
+    description: string
+}
+
+export const aboutSectionInfo: aboutSectionData[] = [
+    {
+        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cPl9Faevjc4AX-WZeO-&_nc_ht=scontent.fsof8-1.fna&oh=00_AfBRFVjVmSBHjWK5pw8Cj8Pn1Lt_qjuEhyJYwaHX4Cejgg&oe=65FA62F6',
+        title: 'About Me',
+        description: 'Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.'
+    },
+    {
+        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cPl9Faevjc4AX-WZeO-&_nc_ht=scontent.fsof8-1.fna&oh=00_AfBRFVjVmSBHjWK5pw8Cj8Pn1Lt_qjuEhyJYwaHX4Cejgg&oe=65FA62F6',
+        title: 'My Experience',
+        description: 'Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.'
+    },
+    {
+        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cPl9Faevjc4AX-WZeO-&_nc_ht=scontent.fsof8-1.fna&oh=00_AfBRFVjVmSBHjWK5pw8Cj8Pn1Lt_qjuEhyJYwaHX4Cejgg&oe=65FA62F6',
+        title: 'My Hobbies',
+        description: 'I like to spend time in mountains and i enjoy riding offroad. If you are feeling well after the massage feel free to come and ride with me!'
+    },
+];
