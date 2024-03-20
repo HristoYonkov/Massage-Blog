1c425a7965ef1ff92262905548a275481512d4bc
Hristo Yonkov <hristopturs@gmail.com>
2024-03-20 13:00:43 +0200

    changed the new string for images to show

--- app/src/data/aboutSliderData.ts
+++ app/src/data/aboutSliderData.ts
@@ -6,7 +6,7 @@ interface aboutSectionData {
 
 export const aboutSectionInfo: aboutSectionData[] = [
     {
-        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cPl9Faevjc4AX-WZeO-&_nc_ht=scontent.fsof8-1.fna&oh=00_AfBRFVjVmSBHjWK5pw8Cj8Pn1Lt_qjuEhyJYwaHX4Cejgg&oe=65FA62F6',
+        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=oTLLlN3Gq_gAX8YJksR&_nc_ht=scontent.fsof8-1.fna&oh=00_AfDmp9scSFvRhveoOdl551N6nacKUNYyGY-xE4VnD-3BCg&oe=660051B6',
         title: 'About Me',
         description: 'Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.'
     },
