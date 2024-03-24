cf91009e27075d26b1ab606019e1efd16fbf0a0b
Hristo Yonkov <hristopturs@gmail.com>
2024-03-24 14:31:45 +0200

    add viable links

--- app/src/data/aboutSliderData.ts
+++ app/src/data/aboutSliderData.ts
@@ -11,12 +11,12 @@ export const aboutSectionInfo: aboutSectionData[] = [
         description: 'Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.'
     },
     {
-        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cPl9Faevjc4AX-WZeO-&_nc_ht=scontent.fsof8-1.fna&oh=00_AfBRFVjVmSBHjWK5pw8Cj8Pn1Lt_qjuEhyJYwaHX4Cejgg&oe=65FA62F6',
+        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=oTLLlN3Gq_gAX8YJksR&_nc_ht=scontent.fsof8-1.fna&oh=00_AfDmp9scSFvRhveoOdl551N6nacKUNYyGY-xE4VnD-3BCg&oe=660051B6',
         title: 'My Experience',
         description: 'Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.'
     },
     {
-        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cPl9Faevjc4AX-WZeO-&_nc_ht=scontent.fsof8-1.fna&oh=00_AfBRFVjVmSBHjWK5pw8Cj8Pn1Lt_qjuEhyJYwaHX4Cejgg&oe=65FA62F6',
+        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=oTLLlN3Gq_gAX8YJksR&_nc_ht=scontent.fsof8-1.fna&oh=00_AfDmp9scSFvRhveoOdl551N6nacKUNYyGY-xE4VnD-3BCg&oe=660051B6',
         title: 'My Hobbies',
         description: 'I like to spend time in mountains and i enjoy riding offroad. If you are feeling well after the massage feel free to come and ride with me!'
     },
