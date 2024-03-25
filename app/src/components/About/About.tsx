7a56276a734b75ac9cbe7852b5bee849fcbcb942
Hristo Yonkov <hristopturs@gmail.com>
2024-03-25 11:45:11 +0200

    addd functionality for animations slider

--- app/src/components/About/About.tsx
+++ app/src/components/About/About.tsx
@@ -1,33 +1,60 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./About.scss";
 import { aboutSectionInfo } from "@/data/aboutSliderData";
 
 const About: React.FC = () => {
     const [index, setIndex] = useState(0);
+    const [isSlidingRight, setIsSlidingRight] = useState(true);
+    const [animationTrigger, setAnimationTrigger] = useState(false);
+    const sliderContainerRef = useRef<HTMLDivElement>(null);
+
     const info = aboutSectionInfo[index];
 
     const slide = (move: string): void => {
         if (move === '+') {
             if (index + 1 > 2) {
                 setIndex(0);
+                setIsSlidingRight(true);
             } else {
                 setIndex(index + 1);
+                setIsSlidingRight(true);
             }
         }
         if (move === '-') {
             if (index - 1 < 0) {
                 setIndex(2);
+                setIsSlidingRight(false);
             } else {
                 setIndex(index - 1);
+                setIsSlidingRight(false);
             }
         }
+
+        setAnimationTrigger(true);
     }
+
+    useEffect(() => {
+        const animationEndHandler = () => {
+            setAnimationTrigger(false);
+        };
+
+        if (sliderContainerRef.current) {
+            sliderContainerRef.current.addEventListener('animationend', animationEndHandler);
+        }
+
+        return () => {
+            if (sliderContainerRef.current) {
+                sliderContainerRef.current.removeEventListener('animationend', animationEndHandler);
+            }
+        };
+    }, [index]);
+
 
     return (
         <section id="about">
-            <div className="slider-container">
+            <div ref={sliderContainerRef} className={`slider-container ${isSlidingRight ? 'slideRight': 'slideLeft'} ${animationTrigger ? 'animate' : ''}`}>
                 <div className="img-container-about">
                     <img src={info.imgSrc} alt="about-image" />
                 </div>
