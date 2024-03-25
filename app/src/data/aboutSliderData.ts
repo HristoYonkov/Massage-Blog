fe60635c8a7f2f891bdba70fbbd5f3adad1dd6c6
Hristo Yonkov <hristopturs@gmail.com>
2024-03-25 11:46:35 +0200

    Update aboutSliderData.ts

--- app/src/data/aboutSliderData.ts
+++ app/src/data/aboutSliderData.ts
@@ -6,17 +6,17 @@ interface aboutSectionData {
 
 export const aboutSectionInfo: aboutSectionData[] = [
     {
-        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=oTLLlN3Gq_gAX8YJksR&_nc_ht=scontent.fsof8-1.fna&oh=00_AfDmp9scSFvRhveoOdl551N6nacKUNYyGY-xE4VnD-3BCg&oe=660051B6',
+        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ynneooMlaeMAX8YsMpB&_nc_ht=scontent.fsof8-1.fna&oh=00_AfA0eSHEjKpWgXScLcgZ_YQ0rZHCzT5O4BSXCmft2ALQ2Q&oe=66064076',
         title: 'About Me',
         description: 'Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.'
     },
     {
-        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=oTLLlN3Gq_gAX8YJksR&_nc_ht=scontent.fsof8-1.fna&oh=00_AfDmp9scSFvRhveoOdl551N6nacKUNYyGY-xE4VnD-3BCg&oe=660051B6',
+        imgSrc: 'https://scontent.fsof11-1.fna.fbcdn.net/v/t39.30808-6/387783150_311797281464671_4308281017703778320_n.jpg?stp=cp6_dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=h18YYGqwsLIAX-UW3SA&_nc_ht=scontent.fsof11-1.fna&oh=00_AfAcnUqO5ail8iWjlwnEOdfnFlwiAbGXBRyNpFP7NePwlw&oe=6607201F',
         title: 'My Experience',
         description: 'Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.'
     },
     {
-        imgSrc: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=oTLLlN3Gq_gAX8YJksR&_nc_ht=scontent.fsof8-1.fna&oh=00_AfDmp9scSFvRhveoOdl551N6nacKUNYyGY-xE4VnD-3BCg&oe=660051B6',
+        imgSrc: 'https://scontent.fsof11-1.fna.fbcdn.net/v/t39.30808-6/414495918_356145687029830_3303627105665333838_n.jpg?stp=cp6_dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=jrDwj6YIGcIAX_TD3YB&_nc_ht=scontent.fsof11-1.fna&oh=00_AfDVOQDCB3DFq2-2DrZ8yX1_Ndy3jicNzo4Jkp85I3GMDg&oe=6606E959',
         title: 'My Hobbies',
         description: 'I like to spend time in mountains and i enjoy riding offroad. If you are feeling well after the massage feel free to come and ride with me!'
     },
