75056dff401e4d74380dbcea17d83808fecd5c24
Hristo Yonkov <hristopturs@gmail.com>
2024-04-01 10:43:52 +0300

    added correct way for handling the state

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -6,16 +6,12 @@ import { useState } from "react";
 import "./Header.scss";
 import logo from '../../images/milen-logo.png';
 
-// interface HeaderProps {
-//     children: React.ReactNode;
-//     className?: string;
-// }
 
 const Header: React.FC = () => {
     const [isChecked, setIsChecked] = useState(false);
 
     const handleCheckedState = () => {
-        setIsChecked(!isChecked);
+        setIsChecked(state => state = !state);
     }
 
     return (
