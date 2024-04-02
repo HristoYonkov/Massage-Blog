cc9357f4dba1005571837326875ba723a60b7620
Hristo Yonkov <hristopturs@gmail.com>
2024-04-02 12:07:30 +0300

    add dynamic data

--- app/src/components/Work/Work.tsx
+++ app/src/components/Work/Work.tsx
@@ -1,8 +1,12 @@
 "use client"
 
+import { workSectionData } from "@/data/workSectionData";
+
 import "./Work.scss";
 
 const Work: React.FC = () => {
+
+    const info = workSectionData;
 
     return (
         // TODO: To map the services-container's!
@@ -10,25 +14,25 @@ const Work: React.FC = () => {
             <h2>Services</h2>
             <div className="services-container">
                 <div className="img-container-services">
-                    <img src="https://scontent.fsof11-1.fna.fbcdn.net/v/t39.30808-6/414495918_356145687029830_3303627105665333838_n.jpg?stp=cp6_dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=jrDwj6YIGcIAX_TD3YB&_nc_ht=scontent.fsof11-1.fna&oh=00_AfDVOQDCB3DFq2-2DrZ8yX1_Ndy3jicNzo4Jkp85I3GMDg&oe=6606E959" />
+                    <img src={`/images/${info[0].imgSrc}`} />
                 </div>
                 <article className="article-container-services">
-                    <h2>Swedish Massage</h2>
+                    <h2>{info[0].title}</h2>
+                    <p>{info[0].description}</p>
+                </article>
+            </div>
+            <div className="services-container">
+                <div className="img-container-services">
+                    <img src={`/images/${info[1].imgSrc}`} />
+                </div>
+                <article className="article-container-services">
+                    <h2>{info[1].title}</h2>
                     <p>Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
                 </article>
             </div>
             <div className="services-container">
                 <div className="img-container-services">
-                    <img src="https://scontent.fsof11-1.fna.fbcdn.net/v/t39.30808-6/387783150_311797281464671_4308281017703778320_n.jpg?stp=cp6_dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=h18YYGqwsLIAX-UW3SA&_nc_ht=scontent.fsof11-1.fna&oh=00_AfAcnUqO5ail8iWjlwnEOdfnFlwiAbGXBRyNpFP7NePwlw&oe=6607201F" />
-                </div>
-                <article className="article-container-services">
-                    <h2>Deep Tissue Massage</h2>
-                    <p>Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
-                </article>
-            </div>
-            <div className="services-container">
-                <div className="img-container-services">
-                    <img src="https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ynneooMlaeMAX8YsMpB&_nc_ht=scontent.fsof8-1.fna&oh=00_AfA0eSHEjKpWgXScLcgZ_YQ0rZHCzT5O4BSXCmft2ALQ2Q&oe=66064076" />
+                    <img src={`/images/${info[2].imgSrc}`} />
                 </div>
                 <article className="article-container-services">
                     <h2>Hot Stone Massage</h2>
@@ -37,7 +41,7 @@ const Work: React.FC = () => {
             </div>
             <div className="services-container">
                 <div className="img-container-services">
-                    <img src="https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/417524460_374351538542578_7663349785761082265_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ynneooMlaeMAX8YsMpB&_nc_ht=scontent.fsof8-1.fna&oh=00_AfA0eSHEjKpWgXScLcgZ_YQ0rZHCzT5O4BSXCmft2ALQ2Q&oe=66064076" />
+                    <img src={`/images/${info[3].imgSrc}`} />
                 </div>
                 <article className="article-container-services">
                     <h2>Suction Cups</h2>
