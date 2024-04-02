fe3e0861c1f30968b52f57c14cac893cecaf030d
Hristo Yonkov <hristopturs@gmail.com>
2024-04-02 11:23:13 +0300

    Create workSectionData.ts

---
+++ app/src/data/workSectionData.ts
@@ -0,0 +1,28 @@
+interface IworkSectionData {
+    imgSrc: string,
+    title: string,
+    description: string
+}
+
+export const workSectionData: IworkSectionData[] = [
+    {
+        imgSrc: '417524460_374351538542578_7663349785761082265_n.jpg',
+        title: 'Swedish Massage',
+        description: 'Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.'
+    },
+    {
+        imgSrc: '417431711_359966519981080_1856160286172967499_n.jpg',
+        title: 'Deep Tissue Massage',
+        description: 'Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.'
+    },
+    {
+        imgSrc: '414495918_356145687029830_3303627105665333838_n.jpg',
+        title: 'Hot Stone Massage',
+        description: 'I like to spend time in mountains and i enjoy riding offroad. If you are feeling well after the massage feel free to come and ride with me!'
+    },
+    {
+        imgSrc: '411476768_347992454511820_4352877292626657046_n.jpg',
+        title: 'Suction Cups',
+        description: 'I like to spend time in mountains and i enjoy riding offroad. If you are feeling well after the massage feel free to come and ride with me!'
+    },
+];
