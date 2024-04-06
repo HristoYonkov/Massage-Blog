58a53ea9295ea4b24808b4a136c49c3090adb430
Hristo Yonkov <hristopturs@gmail.com>
2024-04-06 16:46:30 +0300

    Update Contact.tsx

---
+++ app/src/components/Contact/Contact.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import "./Contact.scss";
+
+const Contact: React.FC = () => {
+
+    return (
+        <section id="contact">
+            <h2>Contact Me</h2>
+            <article className="article-container-contact">
+
+            </article>
+        </section>
+    )
+}
+
+export default Contact;
