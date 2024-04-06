e1d0f445836f43e7f2f6e5344471ba4a335e3127
Hristo Yonkov <hristopturs@gmail.com>
2024-04-06 11:09:09 +0300

    Map trough the items for reducing the code

--- app/src/components/Work/Work.tsx
+++ app/src/components/Work/Work.tsx
@@ -12,42 +12,17 @@ const Work: React.FC = () => {
         // TODO: To map the services-container's!
         <section id="services">
             <h2>Services</h2>
-            <div className="services-container">
-                <div className="img-container-services">
-                    <img src={`/images/${info[0].imgSrc}`} />
+            {info.map((item) => (
+                <div className="services-container">
+                    <div className="img-container-services">
+                        <img src={`/images/${item.imgSrc}`} />
+                    </div>
+                    <article className="article-container-services">
+                        <h2>{item.title}</h2>
+                        <p>{item.description}</p>
+                    </article>
                 </div>
-                <article className="article-container-services">
-                    <h2>{info[0].title}</h2>
-                    <p>{info[0].description}</p>
-                </article>
-            </div>
-            <div className="services-container">
-                <div className="img-container-services">
-                    <img src={`/images/${info[1].imgSrc}`} />
-                </div>
-                <article className="article-container-services">
-                    <h2>{info[1].title}</h2>
-                    <p>Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
-                </article>
-            </div>
-            <div className="services-container">
-                <div className="img-container-services">
-                    <img src={`/images/${info[2].imgSrc}`} />
-                </div>
-                <article className="article-container-services">
-                    <h2>Hot Stone Massage</h2>
-                    <p>Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
-                </article>
-            </div>
-            <div className="services-container">
-                <div className="img-container-services">
-                    <img src={`/images/${info[3].imgSrc}`} />
-                </div>
-                <article className="article-container-services">
-                    <h2>Suction Cups</h2>
-                    <p>Welcome to Relax & Rejuvenate Massage Therapy! I specialize in providing high-quality massage services to help you unwind, destress, and feel your best.</p>
-                </article>
-            </div>
+            ))}
         </section>
 
     )
