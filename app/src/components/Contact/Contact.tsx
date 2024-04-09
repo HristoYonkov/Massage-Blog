8d53875c044d33d5885ed22d10ecaa94827b2d5e
Hristo Yonkov <hristopturs@gmail.com>
2024-04-09 11:19:18 +0300

    Update Contact.tsx

--- app/src/components/Contact/Contact.tsx
+++ app/src/components/Contact/Contact.tsx
@@ -8,7 +8,14 @@ const Contact: React.FC = () => {
         <section id="contact">
             <h2>Contact Me</h2>
             <article className="article-container-contact">
-
+                <div className='image-holder-contact'>
+                    <img src={`/images/email.png`} alt="email" />
+                    <a href="mailto:hristopturs@gmail.com">hristopturs@gmail.com</a>
+                </div>
+                <div className='image-holder-contact'>
+                    <img src={`/images/mobile.png`} alt="mobile" />
+                    <a href="tel: +359 (899) 567-539">+359 (899) 567-539</a>
+                </div>
             </article>
         </section>
     )
